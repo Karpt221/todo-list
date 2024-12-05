6ef39c85b026200a5113436967e12783a0345817
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2024-12-05 16:02:29 +0200

    Refactor ProjectsController to use single array

--- src/modules/ProjectsController.js
+++ src/modules/ProjectsController.js
@@ -2,50 +2,37 @@ import { Project } from "./Project.js";
 
 export class ProjectsController {
     static #instance;
-    #defaultProjectsList = [];
-    #customProjectsList = [];
 
-
+    #projectsList = [];
 
     constructor(defaultProjects) {
         if (ProjectsController.#instance) {
             throw new Error("ProjectsController instance already exists! Use ProjectsController.getInstance().");
         }
-        this.#defaultProjectsList = defaultProjects;
-        this.#defaultProjectsList[0].createToDo("TITLE", "description", "2024-12-04 22:00", "LOW");
-        this.createProject("Project 1");
-        this.#customProjectsList[0].createToDo("title", "description", "2024-12-04 22:00", "HIGH");
-        
-
+        this.#projectsList = defaultProjects;
+        this.createProject('Project 1');
         ProjectsController.#instance = this;
     }
 
     static getInstance() {
         if (!ProjectsController.#instance) {
-            ProjectsController.#instance = new ProjectsController([new Project('Inbox', 'inbox')]);
+            ProjectsController.#instance = new ProjectsController([new Project('Inbox', 'inbox', true)]);
         }
         return ProjectsController.#instance;
     }
 
-    mergeProjects(){
-        return this.defaultProjectsList.concat(this.customProjectsList);
-    }
-
     createProject(project_title) {
-        this.#customProjectsList.push(new Project(project_title));
+        this.#projectsList.push(new Project(project_title));
     }
     editProject(project_idx, project_title) {
-        Object.assign(this.#customProjectsList[project_idx], project_title)
+        Object.assign(this.#projectsList[project_idx], project_title)
     }
     deleteProject(project_idx) {
-        this.#customProjectsList.splice(project_idx, 1);
+        this.#projectsList.splice(project_idx, 1);
     }
 
-    get customProjectsList() {
-        return Object.freeze([...this.#customProjectsList]);
+    get projectsList() {
+        return Object.freeze([...this.#projectsList]);
     }
 
-    get defaultProjectsList() {
-        return Object.freeze([...this.#defaultProjectsList]);
-    }
 }
